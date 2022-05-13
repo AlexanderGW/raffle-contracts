c578e4350990a321e6d3f674bf31581625e29073
alexandergw <alexgw@gmail.com>
2022-05-14 00:33:34 +0100

    Update migration `LottoToken` to `GameBobToken`

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,11 +1,11 @@
 const Oracle = artifacts.require("Oracle");
 const LottoGame = artifacts.require("LottoGame");
-const LottoToken = artifacts.require("LottoToken");
+const GameBobToken = artifacts.require("GameBobToken");
 
 module.exports = function (deployer) {
   deployer.deploy(Oracle).then(x => {
     return deployer.deploy(LottoGame, x.address).then(y => {
-      return deployer.deploy(LottoToken, y.address)
+      return deployer.deploy(GameBobToken, y.address)
     });
   });
 };
